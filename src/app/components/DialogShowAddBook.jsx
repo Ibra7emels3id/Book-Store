9c1752001da99d0295d6978879c64b0085510723
Ibrahem Elsaid <158269891+Ibra7emels3id@users.noreply.github.com>
2024-11-27 18:56:59 +0200

    first commit

--- src/app/components/DialogShowAddBook.jsx
+++ src/app/components/DialogShowAddBook.jsx
@@ -1,9 +1,10 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 import React, { useEffect, useRef, useState } from 'react';
-import { db } from '../../../FirebaseConfig';
+import { db, storage } from '../../../FirebaseConfig';
 import { Button, styled } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import Image from 'next/image';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -17,18 +18,15 @@ const VisuallyHiddenInput = styled('input')({
     width: 1,
 });
 
-
-
 const DialogShowAddBook = ({ showDialog, HandleShowDialog }) => {
     const [formData, setFormData] = useState({})
     const [sortProduct, setSortProduct] = useState([]);
     const [searchText, setSearchText] = useState('');
     const [isOpen, setIsOpen] = useState(false);
     const [isOpenownership, setIsOpenownership] = useState(false);
-
     const dropdownMenuRef = useRef(null);
-
-
+    const [isLoading, setIsLoading] = useState(false);
+    const [formSuccess, setFormSuccess] = useState('');
 
     // Handle Change events
     const handleChangeEvent = (e) => {
@@ -36,8 +34,44 @@ const DialogShowAddBook = ({ showDialog, HandleShowDialog }) => {
     }
 
     // Handel Submit Data Book Store
-    const handleSubmitDataBookStore = (e) => {
+    const handleSubmitDataBookStore = async (e) => {
         e.preventDefault()
+
+        setIsLoading(true)
+        try {
+            // Set Image Store Data
+            const imageRef = ref(storage, `flogImage/${formData.image.name}/${new Date().toISOString()}`)
+            await uploadBytes(imageRef, formData.image)
+            const imageURL = await getDownloadURL(imageRef);
+
+            // Set Pdf Store Data
+            const pdfRef = ref(storage, `flogPdf/${formData.pdf.name}/${new Date().toISOString()}`)
+            await uploadBytes(pdfRef, formData.pdf)
+            const pdfURL = await getDownloadURL(pdfRef);
+
+            const bookStore = await addDoc(collection(db, '/bookStoreClint'), {
+                title: formData.title,
+                numberPages: formData.numberPages,
+                typeFile: formData.typeFile,
+                author: formData.author,
+                language: formData.language,
+                image: imageURL,
+                category: formData.category,
+                ownership: formData.ownership,
+                description: formData.description,
+                pdf: pdfURL,
+                date: new Date().toDateString(),
+                time: new Date().toLocaleTimeString()
+            })
+            console.log("Data Book Store Successfully Added", bookStore.id)
+            HandleShowDialog()
+            setFormSuccess('تم اضافه الكتاب الي قاعده البيانات أنتظر حتي يتم المراجعه من قبل المسؤال')
+        } catch (error) {
+            console.error("Error adding document: ", error);
+            setIsLoading(false)
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     // Handle Toggle
@@ -49,10 +83,6 @@ const DialogShowAddBook = ({ showDialog, HandleShowDialog }) => {
     const toggleDropdownownership = () => {
         setIsOpenownership((prev) => !prev);
     };
-
-
-
-
 
 
     // Get Data from Server 
@@ -76,10 +106,58 @@ const DialogShowAddBook = ({ showDialog, HandleShowDialog }) => {
     }, [])
 
 
+    // Set Success status
+    if (formSuccess) {
+        return <div className="fixed inset-0 p-4 flex flex-wrap justify-center items-center w-full h-full z-[1000] before:fixed before:inset-0 before:w-full before:h-full before:bg-[rgba(0,0,0,0.5)] overflow-auto font-[sans-serif]">
+            <div className="w-full max-w-lg bg-white shadow-lg rounded-lg p-6 relative">
+                <svg
+                    onClick={() => {
+                        setFormSuccess('')
+                    }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-3.5 cursor-pointer shrink-0 fill-gray-400 hover:fill-red-500 float-right"
+                    viewBox="0 0 320.591 320.591"
+                >
+                    <path
+                        d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
+                        data-original="#000000"
+                    />
+                    <path
+                        d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
+                        data-original="#000000"
+                    />
+                </svg>
+                <div className="my-8 text-center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-14 shrink-0 fill-green-500 inline"
+                        viewBox="0 0 512 512"
+                    >
+                        <path
+                            d="M383.841 171.838c-7.881-8.31-21.02-8.676-29.343-.775L221.987 296.732l-63.204-64.893c-8.005-8.213-21.13-8.393-29.35-.387-8.213 7.998-8.386 21.137-.388 29.35l77.492 79.561a20.687 20.687 0 0 0 14.869 6.275 20.744 20.744 0 0 0 14.288-5.694l147.373-139.762c8.316-7.888 8.668-21.027.774-29.344z"
+                            data-original="#000000"
+                        />
+                        <path
+                            d="M256 0C114.84 0 0 114.84 0 256s114.84 256 256 256 256-114.84 256-256S397.16 0 256 0zm0 470.487c-118.265 0-214.487-96.214-214.487-214.487 0-118.265 96.221-214.487 214.487-214.487 118.272 0 214.487 96.221 214.487 214.487 0 118.272-96.215 214.487-214.487 214.487z"
+                            data-original="#000000"
+                        />
+                    </svg>
+                    <h4 className="text-3xl text-gray-800 font-bold mt-4">
+                        تم أرسال البيانات بنجاح
+                    </h4>
+                    <p className="text-lg text-gray-500 leading-relaxed mt-4">
+                        {formSuccess}
+                    </p>
+                </div>
+            </div>
+        </div>
+    }
+
+
     return (
         <>
             <div className={`${showDialog === false ? " hidden" : "fixed"} inset-0 p-4 flex flex-wrap justify-center items-center w-full h-full z-[1000] before:fixed before:inset-0 before:w-full before:h-full before:bg-[rgba(0,0,0,0.5)] overflow-auto font-[sans-serif]`}>
-                <div className="w-full max-w-2xl bg-white shadow-lg rounded-md p-8 relative">
+                <form className="w-full max-w-2xl bg-white shadow-lg rounded-md p-8 relative">
                     <svg
                         onClick={() => {
                             HandleShowDialog()
@@ -271,7 +349,9 @@ const DialogShowAddBook = ({ showDialog, HandleShowDialog }) => {
                                 النتيجة: <span className="font-bold">{formData.category}</span>
                             </p>
                         )}
-                        <textarea onChange={handleChangeEvent} className='h-32 px-3 py-2 w-full mt-4 outline-none focus:outline-none focus:border-green-500 border focus:ring-0' name="description" id="description" placeholder='أدخل التفاصيل الخاصه بالمنتج' />
+                        <textarea onChange={(e) => {
+                            setFormData({ ...formData, description: e.target.value })
+                        }} className='h-32 px-3 py-2 w-full mt-4 outline-none focus:outline-none focus:border-green-500 border focus:ring-0' name="description" id="description" placeholder='أدخل التفاصيل الخاصه بالمنتج' />
                     </div>
                     <button
                         onClick={handleSubmitDataBookStore}
@@ -280,7 +360,7 @@ const DialogShowAddBook = ({ showDialog, HandleShowDialog }) => {
                     >
                         أرسال البيانات
                     </button>
-                </div>
+                </form>
             </div>
 
         </>
